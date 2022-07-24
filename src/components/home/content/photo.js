54da108f9e9887cd33b23618b0250f0da06cd0a7
fares <fares.bensaad@univ-constantine2.dz>
2022-07-24 12:02:34 +0400

    comit

---
+++ src/components/home/content/photo.js
@@ -0,0 +1,14 @@
+import react from "../logos/react.svg";
+import node from "../logos/node.svg";
+import js from "../logos/javascript.svg";
+import css from "../logos/css.svg";
+import github from "../logos/github.svg";
+import html from "../logos/html.svg";
+import xd from "../logos/xd.svg";
+import db from "../logos/db.png";
+
+
+
+const images= {react,node,js,css,github,html,xd,db} ;
+
+export default images 
